e05156a59596be2932ea11cc540e2373a3ce25f8
dijiflex <dijiflex@gmail.com>
2020-04-08 09:43:26 +0300

    calclulated distances using API aggregation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -108,7 +108,7 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     );
   }
 
-  const radious = 'mi' ? distance / 3963.2 : distance / 6378.1; //mongo expresct the distacne to be in radins
+  const radious = unit === 'mi' ? distance / 3963.2 : distance / 6378.1; //mongo expresct the distacne to be in radins
 
   const tours = await Tour.find({
     startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radious] } }
@@ -121,6 +121,45 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     results: tours.length,
     data: {
       data: tours
+    }
+  });
+});
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    next(
+      new AppError('Please provide lat and long in the format like this', 400)
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1]
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier
+      }
+    },
+    {
+      $project:{
+        distance: 1,
+        name: 1
+      }
+    }
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances
     }
   });
 });
